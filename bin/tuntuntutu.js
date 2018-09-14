743a27433ff3f7f6d1dba74d5e1b7d022201117f
qiushengqiang <qsq11170923@163.com>
2018-09-14 16:07:26 +0800

    init

--- bin/tuntuntutu.js
+++ bin/tuntuntutu.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-
-const program = require('commander')  // npm i commander -D
+const program = require('commander')
 
 program.version('1.0.0')
 	.usage('<command> [项目名称]')
-	.command('init', '创建新项目')
+	.command('init', '新建页面')
 	.parse(process.argv)
